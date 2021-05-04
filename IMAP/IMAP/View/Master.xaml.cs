5c34c97b2691a8455e18c50095e7c9b0365652c2
Егор <mikhieiev99@list.ru>
2021-05-04 16:12:30 +0700

    Replace master menu navigation logic from Master.xaml.cs to MasterPageViewModel.cs

--- IMAP/IMAP/View/Master.xaml.cs
+++ IMAP/IMAP/View/Master.xaml.cs
@@ -9,34 +9,7 @@ namespace IMAP.Views
     {
         public Master()
         {
-            int i = 0;
-
             InitializeComponent();
-
-            MapButton.Clicked += async (sender, e) =>
-            {
-                await App.NavigateMasterDetail(new Detail());
-
-            };
-
-            AboutButton.Clicked += async  (sender, e) =>
-            {
-                await App.NavigateMasterDetail(new About());
-
-            };
-
-            WeatherButton.Clicked += async (sender, e) =>
-            {
-                i++;
-                if (i == 8)
-                {
-                    await App.NavigateMasterDetail(new EasterEgg());
-                }
-                else
-                {
-                    await App.NavigateMasterDetail(new Weather());
-                }               
-            };
         }
     }
 }
